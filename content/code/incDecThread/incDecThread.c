3dc09c916ac28f2a58d0c2b059a7ba7437163658
Ajay Gunalan <ajaygunalan1995@gmail.com>
2022-10-02 12:53:02 +0200

    hmm

--- content/code/incDecThread/incDecThread.c
+++ content/code/incDecThread/incDecThread.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <sched.h>
 
-#define COUNT  1000
+#define COUNT  10
 
 typedef struct
 {
@@ -27,7 +27,7 @@ void *incThread(void *threadp)
 
     for(i=0; i<COUNT; i++)
     {
-        gsum=gsum+i;
+        gsum=gsum+1;
         printf("Increment thread idx=%d, gsum=%d\n", threadParams->threadIdx, gsum);
     }
 }
@@ -40,29 +40,22 @@ void *decThread(void *threadp)
 
     for(i=0; i<COUNT; i++)
     {
-        gsum=gsum-i;
+        gsum=gsum-1;
         printf("Decrement thread idx=%d, gsum=%d\n", threadParams->threadIdx, gsum);
     }
 }
 
 
-
-
 int main (int argc, char *argv[])
 {
-   int rc;
-   int i=0;
+   int rc, i=0;
 
    threadParams[i].threadIdx=i;
-   pthread_create(&threads[i],   // pointer to thread descriptor
-                  (void *)0,     // use default attributes
-                  incThread, // thread function entry point
-                  (void *)&(threadParams[i]) // parameters to pass in
-                 );
+   pthread_create(&threads[i], (void *)0, incThread, (void *)&(threadParams[i]));
    i++;
 
-   threadParams[i].threadIdx=i;
-   pthread_create(&threads[i], (void *)0, decThread, (void *)&(threadParams[i]));
+    threadParams[i].threadIdx=i;
+    pthread_create(&threads[i], (void *)0, decThread, (void *)&(threadParams[i]));
 
    for(i=0; i<2; i++)
      pthread_join(threads[i], NULL);
