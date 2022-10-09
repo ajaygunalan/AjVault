b9efd195a6b628650f26f91ba3699f7a69fb901e
Ajay Gunalan <ajaygunalan1995@gmail.com>
2022-10-09 12:48:27 +0200

    ..

--- code/das/c++/array/validSubsequence.cpp
+++ code/das/c++/array/validSubsequence.cpp
@@ -13,20 +13,17 @@ bool isValidSubsequence(std::vector<int> array, std::vector<int> sequence) {
 }
 
 int main(){
-	std::vector<int> array	= {5, 1, 22, 25, 6, -1, 8, 10};
-	std::vector<int> sequence = {1, 6, -1, 10};
+	std::vector<int> array	= {5, 1, 22, 25, 6, -1, 8, 10}, sequence = {1, 6, -1, 10};
+	
 	std::cout<<"Array = { ";
-	for (auto& value:array) {
+	for (auto& value:array) 
     	std::cout<<value<<" ";
-
-	}
-	std::cout<<"}"<<std::endl;
-	std::cout<<"Sequence = { ";
-	for (auto& value:sequence) {
+	
+	std::cout<<"}"<<std::endl<<"Sequence = { ";
+	for (auto& value:sequence)
     	std::cout<<value<<" ";
-
-	}
 	std::cout<<"}"<<std::endl;
+	
 	if(isValidSubsequence(array, sequence)) 
 		std::cout<<"Is it a valid Subsequence: True"<<std::endl;
 	else 
