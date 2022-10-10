ee27c33d257c4f5fb9bd780f4b4b7495ba8aea99
Ajay Gunalan <ajaygunalan1995@gmail.com>
2022-10-10 08:05:22 +0200

    ..

--- code/das/c++/sort/insertionSort.cpp
+++ code/das/c++/sort/insertionSort.cpp
@@ -1,23 +1,23 @@
-
 #include <iostream>
 #include <vector>
 
 std::vector<int> insertionSort(std::vector<int> array) {
-	for(int i =1; i <= array.size(); i++) {
+	for(int i=1; i <= array.size(); i++) {
 		int index = i;
-		int temp = 0;
-		while(array[index-1] > array[index]) {
-			temp =  array[i];
+		while((index>0) & (array[index] > array[index-1])) {
+		    int temp =  array[index];
+		    array[index] = array[index-1];
 			array[index-1] = temp;
-			index = index - 1;
+			index -= 1;
 		}
-	return array;
 	}
+	return array;
 }
 
 int main(){
 	std::vector<int> array	= {68, 3, 5, 12, 14, 8, 9, 4, 2};
-	
+
+
 	std::cout<<"Unsorted array = { ";
 	for (auto& value:array) 
     	std::cout<<value<<" ";
@@ -31,19 +31,3 @@ int main(){
     	std::cout<<value<<" ";
 	std::cout<<"}"<<std::endl;
 }
-
-
-
-// def insertionSort(arr):
-//     for i in range(1, len(arr)):
-//         index = i
-//         while index > 0 and arr[index - 1] > arr[index]:
-//             temp = arr[index]
-//             arr[index] = arr[index - 1]
-//             arr[index - 1] = temp
-//             index = index - 1
-//     return arr
-
-// arr = []
-// print("Unsorted array is :", arr)
-// print("Sorted array is :", insertionSort(arr))
